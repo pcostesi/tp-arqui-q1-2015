fc9c301095d2b417acc514458ee956f978e9a075
Pablo Alejandro Costesich <rhfixer@gmail.com>
2015-05-28 01:02:54 -0300

    Video driver (first draft)

---
+++ Kernel/video.c
@@ -0,0 +1,143 @@
+#include <video.h>
+
+#define COLS ((VID_COLS) * 2)
+#define ROWS (VID_ROWS)
+
+#define VID_RAW_POS(row, col) ((char *) _vid_video + COLS * (row) + (col))
+#define CHR(c, fmt) ((uint16_t)((c) & 0xFF | (fmt) << 8))
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define TAB_SIZE (8)
+
+static volatile char * _vid_video = (char *) 0xB8000;
+static volatile char * _vid_cursor = (char *) 0xB8000;
+static char _vid_fmt = (char) 0x07;
+
+inline static void vid_scroll();
+inline static void _vid_copy_row(const int row_src, const int row_dst);
+
+
+void vid_raw_print(const char * str, unsigned int n)
+{
+	char * c = (char *) str;
+	char * fmt = (char *) str;
+	while (n--) {
+		vid_raw_putc(*c++, *fmt++);
+	}
+}
+
+void vid_print(const char * str)
+{
+	char * p = (char *) str;
+	while (*p != '\0') {
+		vid_putc(*p++);
+	}
+	
+}
+
+void vid_println(const char * str)
+{
+	vid_print(str);
+	vid_putc('\n');
+}
+
+void vid_clr() {
+	int i;
+	for (i = 0; i < COLS * ROWS ; i++) {
+		*_vid_cursor++ = (char) 0;
+		*_vid_cursor++ = (char) 0;
+	}
+	_vid_cursor = (char *) _vid_video;
+}
+
+char vid_color(const enum VID_COLOR foreground, const enum VID_COLOR background)
+{
+	return _vid_fmt = VID_COLOR(foreground, background);
+}
+
+void vid_putc(const char c)
+{
+	vid_raw_putc(c, _vid_fmt);
+}
+
+void vid__vid_cursor(const unsigned int row, const unsigned int col)
+{
+	int sane_row = MIN(ROWS - 1, row);
+	int sane_col = MIN(COLS, col);
+	_vid_cursor = VID_RAW_POS(sane_row, sane_col);
+}
+
+void vid_raw_putc(const char c, const enum VID_COLOR fmt)
+{
+	int offset, row, col;
+
+	if (_vid_cursor >= VID_RAW_POS(ROWS, 0)) {
+		vid_scroll();
+		vid__vid_cursor(ROWS - 1, 0);
+	}
+
+	offset = _vid_cursor - _vid_video;
+	row = offset / COLS;
+	col = offset % COLS;
+
+	switch (c) {
+		case '\n':
+		while (_vid_cursor < VID_RAW_POS(row + 1, 0)) {
+			*_vid_cursor++ = ' ';
+			*_vid_cursor++ = fmt;
+		}
+		vid__vid_cursor(row + 1, 0);
+		break;
+	
+		case '\r':
+		vid__vid_cursor(row, 0);
+		break;
+
+		case '\b':
+		vid__vid_cursor(row, MAX(col - 1, 0));
+		break;
+
+		case '\t':
+		do {
+			*_vid_cursor++ = ' ';
+			*_vid_cursor++ = fmt;
+			col += 2;
+		} while (col < COLS && col < TAB_SIZE * 2);
+		break;
+	
+		default:
+		*_vid_cursor++ = c;
+		*_vid_cursor++ = fmt;
+	}
+}
+
+inline static void _vid_copy_row(const int row_src, const int row_dst)
+{
+	char * src = VID_RAW_POS(row_src, 0);
+	char * dst = VID_RAW_POS(row_dst, 0);
+	int idx;
+	for (idx = 0; idx < COLS; idx++) {
+		*dst++ = *src++;
+	}
+}
+
+inline static void vid_scroll()
+{
+	int row, col;
+	char * ptr = VID_RAW_POS(ROWS - 1, 0);
+
+	for (row = 0; row < ROWS - 1; row++) {
+		_vid_copy_row(row + 1, row);
+	}
+
+	for (col = 0; col < COLS; col++) {
+		*ptr++ = (char) 0;
+	}
+}
+
+#undef COLS
+#undef ROWS
+#undef MAX
+#undef MIN
+#undef TAB_SIZE
+#undef VID_RAW_POS
