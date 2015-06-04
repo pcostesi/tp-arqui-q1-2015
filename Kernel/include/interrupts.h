8ea1ba934d5b3b6df04f7d51648ca81dac8f3359
Pablo Alejandro Costesich <rhfixer@gmail.com>
2015-06-04 17:27:03 -0300

    bind syscalls to main

--- Kernel/include/interrupts.h
+++ Kernel/include/interrupts.h
@@ -18,32 +18,15 @@
 #define IDTE_HW			(IDTE_PRESENT | IDTE_DPL_HW | IDTE_I_GATE)
 #define IDTE_SW			(IDTE_PRESENT | IDTE_DPL_SW | IDTE_I_GATE)
 
-#define IDT_SIZE		0x80		/*	We should at least handle int80h!	*/
+#define IDT_SIZE		0xFF		/*	We should at least handle int80h!	*/
 
-/* The following structs are packed to reflect the arch registers. 
- * zX fields MUST BE SET TO 0 OR... FIRE AND BRIMSTONE,
- * cats and dogs living together.
- */
-#pragma pack(push)
-#pragma pack (1)
-
-struct IDT_Register {
-	uint16_t	limit;
-	uint64_t	offset;
-};
-
-#pragma pack(pop)
-
-void (* IDT_Handler)(void);
+typedef void (* IDT_Handler)(int irq);
 
 /* I personally find typedefs ugly, as they hide too much details in kernel impl.
  * and break the expectations in forward declaration of structures.
  */
-void install_IDTR(struct IDT_Register * idtr);
-void install_IDT_handler((IDT_Handler *) handler, uint16_t interrupt);
-
-void _cli(void);
-void _sti(void);
-void _halt(void);
+void install_IDTR(void);
+void install_IDT_handler(IDT_Handler * handler, uint16_t interrupt);
+void syscall(void);
 
 #endif
