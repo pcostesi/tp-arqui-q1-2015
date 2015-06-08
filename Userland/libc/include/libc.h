0db51d03bd0c808a852b90a114d488fd79f423e9
Pablo Alejandro Costesich <rhfixer@gmail.com>
2015-06-07 23:18:54 -0300

    add rtctime

--- Userland/libc/include/libc.h
+++ Userland/libc/include/libc.h
@@ -1,7 +1,20 @@
 #ifndef __LIBC
 #define __LIBC 1
 
+/* common structs */
+struct rtc_time 
+{
+	int sec;
+	int min;
+	int hour;
+	int mday;
+	int mon;
+	int year;
+};
+
 /* syscalls */
+extern void gettime(struct rtc_time *);
+extern void settime(struct rtc_time *);
 extern int write(unsigned int fd, char * str, unsigned int size);
 extern void pause(void);
 extern int read(unsigned int fd, char * str, unsigned int size);
